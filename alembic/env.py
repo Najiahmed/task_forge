4e6ec6fa418ff8c81563f63e80f7e9c3e1500137
NAJI Ahmed (renexter) <ahmed.naji-extern@renault.com>
2026-09-22 11:56:08 +0200

    Versioning using Alembic Done

--- alembic/env.py
+++ alembic/env.py
@@ -4,20 +4,12 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-
+from models.tasks import Task
+from database.database import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
-from database.database import Base, DATABASE_URL
-from models.tasks import Task
-
-
-config.set_main_option(
-    "sqlalchemy.url",
-    str(DATABASE_URL).replace("%", "%%")
-)
-
-
+config = context.config
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
--- models/tasks.py
+++ models/tasks.py
@@ -1,10 +1,22 @@
-from database.database import Base
+from typing import Optional
+
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import String
+
+from database.database import Base
+
 
 class Task(Base):
     __tablename__ = "tasks"
 
-    id: Mapped[int] = mapped_column(primary_key=True, index=True)
-    title: Mapped[str] = mapped_column(String(100), index=True)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(100))
     completed: Mapped[bool] = mapped_column(default=False)
+
+    priority: Mapped[Optional[int]] = mapped_column(
+        nullable=True
+        )
+
+    meta_data: Mapped[Optional[str]] = mapped_column(
+        nullable=True
+    )
